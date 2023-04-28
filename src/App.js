4866b2886a76bedbd44eb5a6208051e64f67ad39
anis <medanis.lakhdar@gmail.com>
2023-04-28 18:09:54 +0200

    work on main page + setup all features

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Header from './components/header/Header';
+import Main from './pages/main/Main' ;
 import './App.css';
+import SideBar from './components/sideBar/SideBar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className='container'> 
+        <Header />
+        <SideBar />
+        <Routes>
+          <Route path="/" element={<Main />} />
+          {/* <Route path='*' element={<ErrorPage />} /> */}
+        </Routes>
+       
+      </div>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/sideBar/SideBar.jsx
@@ -0,0 +1,8 @@
+import './sideBar.css';
+function SideBar(){
+    return (
+        <div>SideBar</div>
+    )
+}
+
+export default SideBar;
