a4ec780dedd6321309972c3024e663872a84d521
anis <anislakhdar@tutamail.com>
2023-04-28 18:09:54 +0200

    work on main page + setup all features

---
+++ src/pages/main/Main.jsx
@@ -0,0 +1,41 @@
+import React, { useEffect, useState } from "react";
+
+// import ActivityChart from "./components/ActivityChart";
+// import TimeSessionsChart from "./components/TimeSessionsChart";
+// import RadarChart from "./components/RadarChart";
+// import ScoreChart from "./components/ScoreChart";
+// import InfoCard from "./components/InfoCard";
+
+function Main() {
+    
+  //as a prototype, this website need the userID to be set manually
+  const userId = 18; 
+
+  const [infoUser, setInfoUser] = useState(null);
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      try {
+        const response = await fetch(`${process.env.PUBLIC_URL}/mockedDatas/data.json`);
+        const data = await response.json();
+        const userInfo = data.USER_MAIN_DATA.find(user => user.id === 18);
+        setInfoUser(userInfo);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    fetchUser();
+  }, [userId]);
+
+  return (
+ 
+    <div className="main">
+        <div>
+            <p className='main__profileName'>Bonjour <span>{infoUser?.userInfos?.firstName}</span></p>
+            <p className='main__text'>Félicitation ! Vous avez explosé vos objectifs hier 👏</p>
+        </div>
+    </div>
+  );
+}
+
+export default Main;
