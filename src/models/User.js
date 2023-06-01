c6b05288fb9782169ecae3c62d6c9f66d36ebde4
anis <medanis.lakhdar@gmail.com>
2023-06-01 16:01:58 +0200

    work on score chart + add user model and user factory

---
+++ src/models/User.js
@@ -0,0 +1,69 @@
+class User {
+  #userId;
+
+  #firstName;
+
+  #lastName;
+
+  #age;
+
+  #score;
+
+  #calorieCount;
+
+  #proteinCount;
+
+  #carbohydrateCount;
+
+  #lipidCount;
+
+  constructor(data) {
+    this.#userId = data.id;
+    this.#firstName = data.userInfos.firstName;
+    this.#lastName = data.userInfos.lastName;
+    this.#age = data.userInfos.age;
+    this.#score = data.score;
+    this.#calorieCount = data.keyData.calorieCount;
+    this.#proteinCount = data.keyData.proteinCount;
+    this.#carbohydrateCount = data.keyData.carbohydrateCount;
+    this.#lipidCount = data.keyData.lipidCount;
+  }
+
+  get userId() {
+    return this.#userId;
+  }
+
+  get firstName() {
+    return this.#firstName;
+  }
+
+  get lastName() {
+    return this.#lastName;
+  }
+
+  get age() {
+    return this.#age;
+  }
+
+  get score() {
+    return this.#score;
+  }
+
+  get calorieCount() {
+    return this.#calorieCount;
+  }
+
+  get proteinCount() {
+    return this.#proteinCount;
+  }
+
+  get carbohydrateCount() {
+    return this.#carbohydrateCount;
+  }
+
+  get lipidCount() {
+    return this.#lipidCount;
+  }
+}
+
+export default User;
